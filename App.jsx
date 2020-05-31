52d5580a69926584cb989f404d6295775a02a5a7
Mate Patrik <matepatt@gmail.com>
2020-05-31 22:45:30 +0200

    changed Stack.Navigator to Drawer.Navigator

--- App.jsx
+++ App.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import AppStyles from './styles.js';
 import NewPage from "./src/pages/NewPage";
+import TestPage from "./src/pages/TestPage";
 
-const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
       <NavigationContainer style={AppStyles.container}>
-          <Stack.Navigator>
-              <Stack.Screen name='NewPage' component={NewPage} options={{ headerShown: false }} />
-          </Stack.Navigator>
+          <Drawer.Navigator initialRouteName="Home">
+              <Drawer.Screen name="Áttekintés" component={NewPage} />
+              <Drawer.Screen name="Cél beállítása" component={TestPage} />
+              <Drawer.Screen name="Mai teljesítmény" component={NewPage} />
+              <Drawer.Screen name="Beállítások" component={NewPage} />
+              <Drawer.Screen name="Kijelentkezés" component={NewPage} />
+          </Drawer.Navigator>
       </NavigationContainer>
   );
 }
