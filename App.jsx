0388d049518fa89e49d2187c48538ddd8074deb1
Mate Patrik <matepatt@gmail.com>
2020-06-07 03:23:59 +0200

    added new pages in to drawer

--- App.jsx
+++ App.jsx
@@ -7,6 +7,9 @@ import LoginPage from "./src/pages/Login/LoginPage";
 import RegistrationPage from "./src/pages/Registration/RegistrationPage";
 import DailyViewPage from "./src/pages/DailyView/DailyViewPage";
 import SettingsPage from "./src/pages/Settings/SettingsPage";
+import SetGoalPage from "./src/pages/SetGoal/SetGoalPage";
+import NewProductPage from "./src/pages/NewProduct/NewProductPage";
+import OverviewPage from "./src/pages/Overview/OverviewPage";
 
 const Drawer = createDrawerNavigator();
 
@@ -14,13 +17,14 @@ function App() {
   return (
       <NavigationContainer>
           <Drawer.Navigator initialRouteName="Home">
-              <Drawer.Screen name="Áttekintés" component={DailyViewPage} />
-              <Drawer.Screen name="Cél beállítása" component={LoginPage} />
-              <Drawer.Screen name="Mai teljesítmény" component={LoginPage} />
+              <Drawer.Screen name="Áttekintés" component={OverviewPage} />
+              <Drawer.Screen name="Cél beállítása" component={SetGoalPage} />
+              <Drawer.Screen name="Mai teljesítmény" component={DailyViewPage} />
               <Drawer.Screen name="Beállítások" component={SettingsPage} />
               <Drawer.Screen name="Kijelentkezés" component={LoginPage} />
-              {/*TODO kivenni a regisztrációt a drawerből és a login page megfelelő gombjára rakni */}
+              {/*TODO kivenni a regisztrációt és az új terméket a drawerből és a login page megfelelő gombjára rakni */}
               <Drawer.Screen name="Regisztráció" component={RegistrationPage} />
+              <Drawer.Screen name="Új termék" component={NewProductPage} />
           </Drawer.Navigator>
       </NavigationContainer>
   );
